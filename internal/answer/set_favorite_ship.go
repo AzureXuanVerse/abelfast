34b5650a02a9cfdfbbaa49aa712b5a0b36c460d1
ggmolly <molly@molly.sh>
2026-02-21 22:37:52 +0100

    test(answer): cover favorite ship 12040 response paths

--- internal/answer/set_favorite_ship.go
+++ internal/answer/set_favorite_ship.go
@@ -2,9 +2,14 @@ package answer
 
 import (
 	"github.com/ggmolly/belfast/internal/connection"
+	"github.com/ggmolly/belfast/internal/orm"
 	"github.com/ggmolly/belfast/internal/protobuf"
 	"google.golang.org/protobuf/proto"
 )
+
+var setFavoriteShipPreference = func(ship *orm.OwnedShip, flag uint32) error {
+	return ship.SetFavorite(flag)
+}
 
 func SetFavoriteShip(buffer *[]byte, client *connection.Client) (int, int, error) {
 	var data protobuf.CS_12040
@@ -17,7 +22,7 @@ func SetFavoriteShip(buffer *[]byte, client *connection.Client) (int, int, error
 
 	// Check if the ship is in the dock
 	if ship, ok := client.Commander.OwnedShipsMap[data.GetShipId()]; ok {
-		if err := ship.SetFavorite(data.GetFlag()); err != nil {
+		if err := setFavoriteShipPreference(ship, data.GetFlag()); err != nil {
 			response.Result = proto.Uint32(1)
 		}
 	} else {
