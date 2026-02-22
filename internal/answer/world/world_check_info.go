68728057c2ec49aa7ab8ed4ca23058090d246d11
ggmolly <molly@molly.sh>
2026-02-22 14:41:08 +0100

    fix(world): sync operation siren ap regen with reset windows

--- internal/answer/world/world_check_info.go
+++ internal/answer/world/world_check_info.go
@@ -1,6 +1,8 @@
 package world
 
 import (
+	"time"
+
 	"github.com/ggmolly/belfast/internal/connection"
 	"github.com/ggmolly/belfast/internal/orm"
 
@@ -12,6 +14,13 @@ func WorldCheckInfo(buffer *[]byte, client *connection.Client) (int, int, error)
 	runtime, err := orm.LoadOrCreateWorldRuntime(client.Commander.CommanderID)
 	if err != nil {
 		return 0, 33001, err
+	}
+	if changed, _, err := orm.SyncWorldRuntime(runtime, time.Now().UTC()); err != nil {
+		return 0, 33001, err
+	} else if changed {
+		if err := orm.SaveWorldRuntime(runtime); err != nil {
+			return 0, 33001, err
+		}
 	}
 	isWorldOpen := uint32(0)
 	if runtime.MapID != 0 {
